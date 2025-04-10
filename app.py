67e664e77b11a8ad9e30550b27aa34cbd306edcf
Karanveer Singh <129200297+KaranveerSinghCoder@users.noreply.github.com>
2025-04-10 10:41:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ uploaded_file2 = st.file_uploader("Upload Another CSV File (Optional)", type=["c
 if uploaded_file1:
     data1 = pd.read_csv(uploaded_file1)
     st.subheader("Select Columns for Main File")
-    time_col1 = st.selectbox("Select Timestamp Column", data1.columns)
-    value_col1 = st.selectbox("Select Data Column", data1.columns)
+    time_col1 = st.selectbox("Select Timestamp Column", data1.columns, key="time1")
+    value_col1 = st.selectbox("Select Data Column", data1.columns, key="val1")
 
     x1 = data1[time_col1]
     y1 = data1[value_col1]
@@ -30,11 +30,12 @@ if uploaded_file1:
     if uploaded_file2:
         data2 = pd.read_csv(uploaded_file2)
         st.subheader("Select Columns for Second File")
-        time_col2 = st.selectbox("Select Timestamp Column (File 2)", data2.columns)
-        value_col2 = st.selectbox("Select Data Column (File 2)", data2.columns)
+        time_col2 = st.selectbox("Select Timestamp Column (File 2)", data2.columns, key="time2")
+        value_col2 = st.selectbox("Select Data Column (File 2)", data2.columns, key="val2")
 
         x2 = data2[time_col2]
         y2 = data2[value_col2]
+
         fig.add_trace(go.Scatter(x=x2, y=y2, mode='lines', name='2nd Graph', line=dict(color='blue')))
 
     fig.update_layout(title="Interactive Graph", xaxis_title="Timestamp", yaxis_title="Selected Data Column",
@@ -42,7 +43,6 @@ if uploaded_file1:
     st.plotly_chart(fig, use_container_width=True)
 
     st.subheader("Peak Detection & CSV Export (*For Main File Only)")
-
     maxima, _ = find_peaks(y1)
     minima, _ = find_peaks(-y1)
 
@@ -70,29 +70,51 @@ if uploaded_file1:
     fig_peaks.add_trace(go.Scatter(x=maxima_timestamps, y=maxima_values, mode='markers', name="Maxima", marker=dict(color='red', size=8)))
     fig_peaks.add_trace(go.Scatter(x=minima_timestamps, y=minima_values, mode='markers', name="Minima", marker=dict(color='blue', size=8)))
 
-    fig_peaks.update_layout(title="Interactive Peak Detection (*For Main File Only)", xaxis_title="Timestamp",
+    fig_peaks.update_layout(title="Peak Detection Graph", xaxis_title="Timestamp",
                             yaxis_title="Value", hovermode="x", template="plotly_dark")
     st.plotly_chart(fig_peaks, use_container_width=True)
 
     st.subheader("FFT Analysis of IMU Data")
     sampling_frequency = st.number_input("Enter Sampling Frequency (Hz)", min_value=1, max_value=10000, value=2000, step=1)
 
-    N = len(y1)
-    fft_result = np.fft.fft(y1)
-    freqs = np.fft.fftfreq(N, 1/sampling_frequency)
-    magnitude = np.abs(fft_result)
-    scaled_magnitude = 10 * np.log10(magnitude)
+    N1 = len(y1)
+    fft_result1 = np.fft.fft(y1)
+    freqs1 = np.fft.fftfreq(N1, 1/sampling_frequency)
+    magnitude1 = np.abs(fft_result1)
+    scaled_magnitude1 = 10 * np.log10(magnitude1 + 1e-10)
 
-    fft_table = pd.DataFrame({'Frequency (Hz)': freqs[:N//2], 'Amplitude (dB)': scaled_magnitude[:N//2]})
-    st.download_button("Download FFT CSV", fft_table.to_csv(index=False), "fft_analysis_results.csv", "text/csv")
+    fft_table1 = pd.DataFrame({'Frequency (Hz)': freqs1[:N1//2], 'Amplitude (dB)': scaled_magnitude1[:N1//2]})
+    st.download_button("Download FFT CSV (Main File)", fft_table1.to_csv(index=False), "fft_main.csv", "text/csv")
 
-    fig_fft = go.Figure()
-    fig_fft.add_trace(go.Scatter(x=freqs[:N//2], y=scaled_magnitude[:N//2], mode='lines', name='FFT Magnitude Spectrum'))
+    fig_fft1 = go.Figure()
+    fig_fft1.add_trace(go.Scatter(x=freqs1[:N1//2], y=scaled_magnitude1[:N1//2], mode='lines', name='Main File'))
+    fig_fft1.update_layout(title="FFT Analysis - Main File", xaxis_title="Frequency (Hz)",
+                           yaxis_title="Amplitude (dB)", hovermode="x", template="plotly_dark")
+    st.plotly_chart(fig_fft1, use_container_width=True)
 
-    fig_fft.update_layout(title="FFT Analysis (Magnitude Spectrum)", xaxis_title="Frequency (Hz)",
-                          yaxis_title="Amplitude (dB)", hovermode="x", template="plotly_dark")
-    st.plotly_chart(fig_fft, use_container_width=True)
+    if uploaded_file2:
+        N2 = len(y2)
+        fft_result2 = np.fft.fft(y2)
+        freqs2 = np.fft.fftfreq(N2, 1/sampling_frequency)
+        magnitude2 = np.abs(fft_result2)
+        scaled_magnitude2 = 10 * np.log10(magnitude2 + 1e-10)
 
+        fft_table2 = pd.DataFrame({'Frequency (Hz)': freqs2[:N2//2], 'Amplitude (dB)': scaled_magnitude2[:N2//2]})
+        st.download_button("Download FFT CSV (Second File)", fft_table2.to_csv(index=False), "fft_secondary.csv", "text/csv")
+
+        fig_fft2 = go.Figure()
+        fig_fft2.add_trace(go.Scatter(x=freqs2[:N2//2], y=scaled_magnitude2[:N2//2], mode='lines', name='Second File'))
+        fig_fft2.update_layout(title="FFT Analysis - Second File", xaxis_title="Frequency (Hz)",
+                               yaxis_title="Amplitude (dB)", hovermode="x", template="plotly_dark")
+        st.plotly_chart(fig_fft2, use_container_width=True)
+
+        # Combined FFT plot
+        fig_combined = go.Figure()
+        fig_combined.add_trace(go.Scatter(x=freqs1[:N1//2], y=scaled_magnitude1[:N1//2], mode='lines', name='Main File'))
+        fig_combined.add_trace(go.Scatter(x=freqs2[:N2//2], y=scaled_magnitude2[:N2//2], mode='lines', name='Second File'))
+        fig_combined.update_layout(title="Combined FFT Analysis", xaxis_title="Frequency (Hz)",
+                                   yaxis_title="Amplitude (dB)", hovermode="x", template="plotly_dark")
+        st.plotly_chart(fig_combined, use_container_width=True)
 
 
 
